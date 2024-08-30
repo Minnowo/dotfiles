5c68bf7d66a54cd7be240d90f5bd2db088ca81e3
Minnowo <62451415+Minnowo@users.noreply.github.com>
2024-08-30 09:44:56 -0400

    update 2024-08-30

--- .config/nvim/lua/minno/core/remap.lua
+++ .config/nvim/lua/minno/core/remap.lua
@@ -36,8 +36,14 @@ vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", { silent = true })
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { silent = true })
 vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", { silent = true })
 
-vim.keymap.set('n', '<C-S-l>', ':vertical resize -2<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-S-h>', ':vertical resize +2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-S-l>', ':vertical resize -2<CR>', { noremap = true, silent = true, desc="Resize vertical smaller"})
+vim.keymap.set('n', '<C-S-h>', ':vertical resize +2<CR>', { noremap = true, silent = true, desc="Resize vertical bigger" })
+
+-- works in tmux
+vim.keymap.set('n', '<C-left>', ':vertical resize -2<CR>', { noremap = true, silent = true, desc="Resize vertical smaller"})
+vim.keymap.set('n', '<C-right>', ':vertical resize +2<CR>', { noremap = true, silent = true, desc="Resize vertical bigger" })
+vim.keymap.set('n', '<C-up>', ':horizontal resize -2<CR>', { noremap = true, silent = true, desc="Resize horizontal smaller"})
+vim.keymap.set('n', '<C-down>', ':horizontal resize +2<CR>', { noremap = true, silent = true, desc="Resize horizontal bigger" })
 
 vim.keymap.set('n', 'H', 'gT')
 vim.keymap.set('n', 'L', 'gt')
@@ -48,10 +54,10 @@ opts.desc = "Replace all of current word"
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], opts)
 
 
-opts.desc = "Run selected lua code"
-vim.keymap.set("v", "<C-x>", [[<esc>:'<,'>:w !lua<CR>]], opts)
+-- opts.desc = "Run selected lua code"
+-- vim.keymap.set("v", "<C-x>", [[<esc>:'<,'>:w !lua<CR>]], opts)
 
 -- run selected lua code and open the output in a new buffer
-opts.desc = "Run selected lua code and open output in new buffer"
-vim.keymap.set("v", "<C-S-x>", [[<esc>:'<,'>w !lua >/tmp/out.txt<CR> :vsp /tmp/out.txt<CR>]], opts)
+-- opts.desc = "Run selected lua code and open output in new buffer"
+-- vim.keymap.set("v", "<C-S-x>", [[<esc>:'<,'>w !lua >/tmp/out.txt<CR> :vsp /tmp/out.txt<CR>]], opts)
 
